f54ad0f442490f71b553df0562b25bedd91f3089
Leonardo <yleeoembd@gmail.com>
2023-01-29 14:55:04 -0300

    feat: tarefa finalizada com todas features incluidas

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import { ListTechnologies } from "./components/listTechnologies";
+import API from "./api";
+function App() {
+  const [technologies, setTechnologies] = useState([]);
+
+  useEffect(() => {
+    API.get("/api/technology/")
+      .then((res) =>
+        setTechnologies(res.data.sort((a, b) => a.index - b.index))
+      )
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    setTechnologies(
+      technologies.sort((a, b) => {
+        return a.index - b.index;
+      })
+    );
+  }, [technologies]);
+
+  // console.log(technologies);
+  return (
+    <main className="App">
+      <ListTechnologies
+        technologies={technologies}
+        setTechnologies={setTechnologies}
+      />
+    </main>
+  );
+}
+
+export default App;
