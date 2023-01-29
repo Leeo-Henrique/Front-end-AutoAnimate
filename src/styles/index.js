f1aeeba6e35a950744ef3ea84e23ff55309992d0
Leonardo <yleeoembd@gmail.com>
2023-01-29 14:47:53 -0300

    feat: create a globa style

---
+++ src/styles/index.js
@@ -0,0 +1,12 @@
+import styled, { createGlobalStyle, css } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root{
+    --mainColor : #deceff;
+    --alternativeCollor: #ece2ff;
+    --buttonColor : #4711de;
+    --fontColor: #484444;
+    --closeButton: #ff0000;
+    --purple-md: #350b5a;
+  }
+`;
